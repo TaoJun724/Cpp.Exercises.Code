880f7dedfb50a72451a70807932629316e5ae5ef
TaoJun724 <TaoJun724@gmail.com>
2019-06-24 22:36:59 +0800

    day day up

---
+++ v27.cc
@@ -0,0 +1,48 @@
+#include<iostream>  
+using namespace std;  
+   
+#define MAX 1024  
+   
+int N;  
+int column[MAX];//每行对应的列值  
+   
+int sum=0;  
+bool Place(int row,int col)//判断是否该行可以放置皇后  
+{  
+    int i=1;  
+    for(i;i<row;i++)  
+    {  
+        if((column[i]-col)==(i-row)||(column[i]-col)==(row-i)||column[i]==col)  
+            return false;  
+    }  
+    return true;  
+}  
+   
+void backTrace(int k)//回溯函数  
+{  
+    int i;  
+    if(k>N)  
+    {  
+        cout<<"路径"<<++sum<<"序列为: ";  
+        for(i=1;i<=N;i++)  
+            cout<<column[i]<<" ";  
+        cout<<endl;  
+    }  
+    else 
+    {  
+        for(i=1;i<=N;i++)  
+        {  
+            if(Place(k,i))  
+            {  
+                column[k]=i;  
+                backTrace(k+1);  
+            }  
+        }  
+    }  
+}  
+int main()  
+{  
+    cout<<"输入皇后数:";  
+    cin>>N;  
+    backTrace(1);  
+}
